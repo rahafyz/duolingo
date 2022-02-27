f601045b5340287ce2a4137e20482967dfa4b711
rahafyz <r.fayaz1998@gmail.com>
2022-02-27 19:08:00 +0330

    add course to user

--- src/main/java/com/mapsa/duolingo/courseUser/CUMapper.java
+++
@@ -1,8 +0,0 @@
-package com.mapsa.duolingo.courseUser;
-
-import com.mapsa.duolingo.common.GenericModelMapper;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface CUMapper extends GenericModelMapper<CourseUser, CourseUserDto> {
-}
